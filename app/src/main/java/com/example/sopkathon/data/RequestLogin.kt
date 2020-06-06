4ded3fd7dbd8c139afbdfd59059f41201f6a7cd6
kym1924 <kym1924@naver.com>
2020-06-07 03:19:21 +0900

    add Register

---
+++ app/src/main/java/com/example/sopkathon/data/RequestLogin.kt
@@ -0,0 +1,6 @@
+package com.example.sopkathon.data
+
+data class RequestLogin (
+    val id : String,
+    val password : String
+)
---
+++ app/src/main/java/com/example/sopkathon/network/RequestRegisterToServer.kt
@@ -0,0 +1,14 @@
+package com.example.sopkathon.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+//싱글톤. 코드 아무데서나 접근 가능하고, 객체는 단 하나만 -> 메모리 효율
+object RequestRegisterToServer {
+    var retrofit = Retrofit.Builder()
+        .baseUrl("http://13.209.144.115:3333")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    var service: RequestRegisterInterface = retrofit.create(RequestRegisterInterface::class.java)
+}
