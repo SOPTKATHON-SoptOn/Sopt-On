9196f4146d24fdca3c0a1541e749cf3b03e43af0
shalo1040 <kimd02@pfw.edu>
2020-06-07 04:55:14 +0900

    answer, check activity

--- app/src/main/java/com/example/sopkathon/AnswerActivity.kt
+++ app/src/main/java/com/example/sopkathon/AnswerActivity.kt
@@ -1,12 +1,21 @@
 package com.example.sopkathon
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_answer.*
+import kotlinx.android.synthetic.main.activity_answer_admin.*
 
 class AnswerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_answer)
+
+        btn_submit.setOnClickListener() {
+            btn_submit.setBackgroundResource(R.drawable.round_green)
+            btn_submit.setTextColor(Color.WHITE)
+        }
     }
 }
