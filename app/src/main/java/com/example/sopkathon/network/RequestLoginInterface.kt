b5a3ff4742299948df3a7d6d1be32126fdffba61
leesujung-dodung <leesujung0917@gmail.com>
2020-06-07 04:22:27 +0900

    check admin ing3

--- app/src/main/java/com/example/sopkathon/network/RequestLoginInterface.kt
+++ app/src/main/java/com/example/sopkathon/network/RequestLoginInterface.kt
@@ -1,12 +1,14 @@
 package com.example.sopkathon.network
 
+import android.telecom.Call
 import com.example.sopkathon.data.RequestLogin
 import com.example.sopkathon.data.ResponseLogin
+import com.example.sopkathon.data.ResponseRegister
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface RequestLoginInterface {
-    @POST("/user/signin")
-    fun requestLogin(@Body body : RequestLogin) : Call<ResponseLogin>
+    @Post("/user/signin")
+    fun requestLogin(@Body body : RequestLogin) : Call<ResponseRegister>
 }
