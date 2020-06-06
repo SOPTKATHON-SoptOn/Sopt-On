e58066ee8cead4d61ce6192d0bfd1ce04f72db66
leesujung-dodung <leesujung0917@gmail.com>
2020-06-06 23:21:26 +0900

    check admin ing

---
+++ app/src/main/java/com/example/sopkathon/CheckAdminActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sopkathon
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CheckAdminActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_admin)
+    }
+}
