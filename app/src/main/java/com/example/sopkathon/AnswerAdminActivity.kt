9798fecebd7c44de650f6a84f84abed58f75addb
shalo1040 <kimd02@pfw.edu>
2020-06-07 07:20:28 +0900

    view + server

--- app/src/main/java/com/example/sopkathon/AnswerAdminActivity.kt
+++ app/src/main/java/com/example/sopkathon/AnswerAdminActivity.kt
@@ -5,17 +5,29 @@ import android.app.TimePickerDialog
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.example.sopkathon.data.RequestCode
+import com.example.sopkathon.network.RequestToServer
+import kotlinx.android.synthetic.main.activity_answer.*
 import kotlinx.android.synthetic.main.activity_answer_admin.*
 import kotlinx.android.synthetic.main.activity_answer_admin.tv_date
 import java.util.*
 
 
 class AnswerAdminActivity : AppCompatActivity() {
+    val requestToServer = RequestToServer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.example.sopkathon.R.layout.activity_answer_admin)
+
+        Glide.with(this).load(R.drawable.adminadd_img_profileimg).into(iv_profile)
+
+
+        var isDateSet = false
+        var isTimeSet = false
 
         val c = Calendar.getInstance()
         val year = c.get(Calendar.YEAR)
@@ -28,6 +40,7 @@ class AnswerAdminActivity : AppCompatActivity() {
         setDateLayout.setOnClickListener() {
             val datePicker = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, month, day ->
                 tv_date.setText(""  + year + "." + (month+1) + "." + day)
+                isDateSet = true
             }, year, month, day)
             datePicker.show()
         }
@@ -36,16 +49,32 @@ class AnswerAdminActivity : AppCompatActivity() {
         linearLayout.setOnClickListener() {
             val timePicker = TimePickerDialog(this, TimePickerDialog.OnTimeSetListener { view, hour, minute ->
                 tv_time.setText("" + hour + ":" + minute)
+                isTimeSet = true
             }, hour, minute, false)
             timePicker.show()
         }
 
         //완료 버튼 클릭
         btn_finish.setOnClickListener() {
-            btn_finish.setBackgroundResource(R.drawable.round_green)
-            btn_finish.setTextColor(Color.WHITE)
-            startActivity(Intent(this, CheckAdminActivity::class.java))
+            if(isDateSet && isTimeSet && ev_answer.text.isNotEmpty()) {
+                requestToServer.service.requestCode(
+                    RequestCode(
+                        seminar_date = "2020-04-18",
+                        seminar_time = "14:00:00",
+                        seminar_code = "hi"
+                    )
+                ).customEnqueue(
+                    onError = {showToast("error")},
+                    onSuccess = {
+                        if(it.success) {
+                            showToast("코드가 생성되었습니다")
+                            btn_finish.setBackgroundResource(R.drawable.round_green)
+                            btn_finish.setTextColor(Color.WHITE)
+                            startActivity(Intent(this, CheckAdminActivity::class.java))
+                        }
+                    }
+                )
+            }
         }
     }
-
 }
